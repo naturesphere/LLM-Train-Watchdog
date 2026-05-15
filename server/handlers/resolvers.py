ade66ff944227c9ceaf1f3551a96797285b42388
keyboardmaster <xxx_bbbb@yeah.net>
2026-05-15 19:40:09 +0800

    debug: aliyun log file

--- server/handlers/resolvers.py
+++ server/handlers/resolvers.py
@@ -24,17 +24,28 @@ class X10000Resolver(Middleware):
             context.data["log_file"] = relative_path
 
 
-class AliYunDSWResolver(Middleware): 
+
+class AliYunDSWResolver(X10000Resolver): 
     def process(self, context):
         """根据通配符模式寻找最新的日志文件"""
-        pattern = os.path.join(context.args.base_dir, context.args.pattern)
-        # print(pattern)
-        log_files = glob.glob(pattern)
+        pattern = os.path.join(context.args.base_dir, '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]') #20260511-2209
+        log_dirs = glob.glob(pattern)
+        if not log_dirs: return
         
-        if not log_files: return
-        
+        log_dirs.sort(key=self.natural_keys, reverse=True)
+        latest_dir = log_dirs[0]
+
+        pattern2 = os.path.join(latest_dir, 'worker_*')
+        worker_folders = glob.glob(pattern2)
+        worker_folders.sort(key=self.natural_keys, reverse=True)
+        latest_worker = worker_folders[0]
+
+        pattern3 = os.path.join(latest_worker, 'none_*/attempt_0/7/stdout.log')
+        log_files = glob.glob(pattern3)
         # 按最后修改时间排序，取最新的一个
         log_files.sort(key=os.path.getmtime, reverse=True)
-        context.current_log_path = log_files[0]
+        log_file = log_files[0]
+        print(log_file)
+        context.current_log_path = log_file
         relative_path = context.current_log_path.replace(context.args.base_dir, '').lstrip('/')
         context.data["log_file"] = relative_path
